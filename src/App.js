02ce50f7d2999b7567149dc07971423264b1cc11
MarcinMaster <m.luba505@gmail.com>
2022-05-09 08:46:03 +0200

    Adding a useEffect to manage API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import SearchBar from "./components/SearchBar";
+
+const api = {
+  base: 'https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=INR'
+}
 
 function App() {
+
+  const [crypto, setCrypto] = useState([])
+  const [search, setSearch] = useState('')
+
+useEffect(() => {
+  fetch(`${api.base}`)
+  .then(res => res.json())
+  .then(result => {
+    setCrypto(result)
+    console.log(result)
+  })
+}, [])
+
+console.log(search)
+
   return (
     <div>
-      Hello World
+      <SearchBar setSearch={setSearch} />
     </div>
   );
 }
